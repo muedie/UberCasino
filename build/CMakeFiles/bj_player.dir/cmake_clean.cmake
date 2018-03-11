7612d42efed849c34d59785ca4fe76bb5b9e96f7
Roshan Shrestha <roshan.shrestha@mavs.uta.edu>
2018-03-11 07:11:28 -0500

    cmake done

---
+++ build/CMakeFiles/bj_player.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bj_player.dir/src/login.cpp.o"
+  "CMakeFiles/bj_player.dir/src/main.cpp.o"
+  "bj_player.pdb"
+  "bj_player"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bj_player.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
