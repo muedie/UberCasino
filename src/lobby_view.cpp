6b5bc02c769c3e6e843aeb875482b33ae4e946d5
Roshan Shrestha <roshan.shrestha@mavs.uta.edu>
2018-03-15 16:14:16 -0500

    build folder clean

--- src/lobby_view.cpp
+++ src/lobby_view.cpp
@@ -5,7 +5,7 @@ Lobby_view::Lobby_view() : Fl_Window(960, 635, "Lobby")
   begin();
   resizable();
 
-  background_img = new Fl_JPEG_Image("images/2.jpg");
+  background_img = new Fl_JPEG_Image("/home/student/Desktop/CSE3310/Project/extra/images/2.jpg");
   main_box = new Fl_Box(0, 0, 960, 640);
   main_box->image(background_img);
 
